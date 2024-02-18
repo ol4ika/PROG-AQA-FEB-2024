b869520beb63cc571ab285007f84044d523de388
ol4ika <ol4ika23@gmail.com>
2024-02-18 22:36:55 +0200

    Homework3

--- src/main/java/org/prog/cycles/CyclesDemo.java
+++ src/main/java/org/prog/cycles/CyclesDemo.java
@@ -58,6 +58,25 @@ public class CyclesDemo {
         }
     }
 
+    private static void monthY(int month) {
+        switch (month) {
+            case 12, 1, 2:
+                System.out.println("Winter");
+                break;
+            case 3, 4, 5:
+                System.out.println("Spring");
+                break;
+            case 6, 7, 8:
+                System.out.println("Summer");
+                break;
+            case 9, 10, 11:
+                System.out.println("Autumn");
+                break;
+            default:
+                System.out.println("Error");
+        }
+    }
+
     private static boolean falseFlag() {
         return false;
     }
